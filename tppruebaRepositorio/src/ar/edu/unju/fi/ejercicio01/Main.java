f67473e267217653df9a3ae3417e244c19209c01
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-04-15 20:30:10 -0300

    prueba

---
+++ tppruebaRepositorio/src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio01;
+
+//Punto 1
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int primerNumero = 8;
+		int segundoNumero = 5;
+		int tercerNumero = 1;
+		int cuartoNumero = 20;
+		int quintoNumero = 9;
+		double promedio = (double) (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println("El promedio es: "+ promedio );
+		System.out.println("Numero1: "+ primerNumero+" --Numero2: "+ segundoNumero+" --Numero3: "+ tercerNumero+" --Numero4: "+ cuartoNumero+" --Numero5: "+ quintoNumero);
+		
+	}
+
+}
